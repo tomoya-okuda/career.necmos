77cbd8e29b390d1ed81a30af7b006f48189a1532
shimokita-bros <tomoyaokuda.info@gmail.com>
2023-04-23 15:25:26 +0900

    ♻️ Headerコンポーネント作成

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,18 @@
+import styles from 'src/components/componentsCSS/Header.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className={styles.header}>
+      <Link href="/" className={styles.menu}>
+        <Image src="icons/logo-black.svg" alt="logo" width={32} height={32} />
+      </Link>
+      <Link href="/" className={styles.menu}>
+        Menu
+      </Link>
+    </header>
+  );
 }
 
 export default Header;
