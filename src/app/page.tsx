7bcd514664708175c613e667ae6886a193d81b67
shimokita-bros <tomoyaokuda.info@gmail.com>
2023-04-23 16:19:15 +0900

    ♻️ @next/fontでNoto Sans JPをインストール

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.scss';
+import FV from '@/components/FV';
 
 export default function Home() {
   return (
     <main>
-      <h1>AAAAA</h1>
+      <FV />
     </main>
-  )
+  );
 }
---
+++ src/components/FV.tsx
@@ -0,0 +1,16 @@
+import styles from 'src/components/componentsCSS/FV.module.scss';
+
+function FV() {
+  return (
+    <div className={styles.fv}>
+      <div className={styles.texts}>
+        <h1>
+          仕事を<br></br>もっと自由に
+        </h1>
+        <p>NECMOS CAREERS</p>
+      </div>
+    </div>
+  );
+}
+
+export default FV;
