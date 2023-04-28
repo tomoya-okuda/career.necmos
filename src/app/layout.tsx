1201c7dba4ff85cc3499b108265e065a841f76a2
tomoya-okuda <tomoyaokuda.info@gmail.com>
2023-04-28 12:13:42 +0900

    ♻️ Metadataの追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,9 +9,55 @@ const notojp = Noto_Sans_JP({
   display: 'swap',
 });
 
+// Metadata
+const siteName = 'Necmos採用サイト';
+const description = 'サイトの説明';
+const url = 'https://career.necmos';
+const ogImageUrl = 'https://OG画像のURL';
+
 export const metadata = {
-  title: 'Necmos採用サイト',
-  description: 'Necmos Recruit Website',
+  title: {
+    default: siteName,
+    /** `next-seo`の`titleTemplate`に相当する機能 */
+    template: `%s - ${siteName}`,
+  },
+  description,
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
+    // ドキュメントを参照して適宜増やすこと
+  },
+  openGraph: {
+    title: siteName,
+    description,
+    url,
+    siteName,
+    images: [
+      {
+        url: ogImageUrl,
+        width: 1800,
+        height: 1600,
+        alt: '画像の説明文',
+      },
+    ],
+    locale: 'ja_JP',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteName,
+    description,
+    site: '@サイト用アカウントのTwitterID',
+    creator: '@作者のTwitterID',
+    images: [ogImageUrl],
+  },
+  verification: {
+    google: 'サーチコンソールのやつ',
+  },
+  alternates: {
+    canonical: url,
+  },
 };
 
 export default function RootLayout({
