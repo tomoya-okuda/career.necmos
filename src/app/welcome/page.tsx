dcd3d964809b0ae2b27f0e70934bcd5154f8fc6f
tomoya-okuda <tomoyaokuda.info@gmail.com>
2023-04-28 13:29:01 +0900

    ♻️ 見出しコンポーネント追加

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,7 +1,12 @@
+import HeadingH3 from '@/components/HeadingH3';
+import styles from 'src/app/welcome/welcome.module.scss';
+
 function page() {
   return (
-    <div>I am Welcome page</div>
-  )
+    <div className={styles.welcome}>
+      <HeadingH3 title="ようこそ、Necmosへ。" color="white" />
+    </div>
+  );
 }
 
-export default page
+export default page;
