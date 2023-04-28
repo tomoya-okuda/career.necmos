a36eb09aa442c579e798dad5ccb3f7c16968a99c
tomoya-okuda <tomoyaokuda.info@gmail.com>
2023-04-28 13:10:36 +0900

    ♻️ よくある質問ページ作成

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,7 +1,12 @@
+import styles from 'src/app/questions/questions.module.scss';
+import HeadingH2 from '@/components/HeadingH3';
+
 function page() {
   return (
-    <div>I am Questions page</div>
-  )
+    <div className={styles.questions}>
+      <HeadingH2 title="よくある質問" />
+    </div>
+  );
 }
 
-export default page
+export default page;
