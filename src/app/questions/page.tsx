58eec71aec559b024158f0cb1b9c5de2c8df3bf3
tomoya-okuda <tomoyaokuda.info@gmail.com>
2023-04-28 13:25:20 +0900

    ♻️ 見出しコンポーネントにカラープロパティ追加

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,10 +1,10 @@
 import styles from 'src/app/questions/questions.module.scss';
-import HeadingH2 from '@/components/HeadingH3';
+import HeadingH3 from '@/components/HeadingH3';
 
 function page() {
   return (
     <div className={styles.questions}>
-      <HeadingH2 title="よくある質問" />
+      <HeadingH3 title="よくある質問" color="black" />
     </div>
   );
 }
--- src/components/HeadingH3.tsx
+++ src/components/HeadingH3.tsx
@@ -1,15 +1,11 @@
 import styles from 'src/components/styles/HeadingH3.module.scss';
-
-type Props = {
-  title?: string;
-};
-
-function HeadingH3(props: Props) {
-  return (
-    <div>
-      <h3 className={styles.h3}>{props.title}</h3>
-    </div>
-  );
+interface HeadingH3Props {
+  title: string;
+  color: 'black' | 'white';
 }
+
+const HeadingH3: React.FC<HeadingH3Props> = ({ title, color }) => {
+  return <h3 className={`${styles.heading} ${styles[color]}`}>{title}</h3>;
+};
 
 export default HeadingH3;
