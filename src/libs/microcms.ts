73f454d8548b91078d4ec6cb9592dc3c741e90c4
tomoya-okuda <tomoyaokuda.info@gmail.com>
2023-04-29 21:59:37 +0900

    ♻️ customFetchオプション（1分毎に値が変わるキャッシュクリア用のパラメータ）

--- src/libs/microcms.ts
+++ src/libs/microcms.ts
@@ -25,6 +25,15 @@ if (!process.env.MICROCMS_API_KEY) {
 export const client = createClient({
   serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
   apiKey: process.env.MICROCMS_API_KEY,
+  customFetch: (input, init) => {
+    if (typeof input === 'string') {
+      const newInput = new URL(input);
+      const time = new Date();
+      newInput.searchParams.set('cacheclearparam', `${time.getMinutes()}`);
+      return fetch(newInput.href, init);
+    }
+    return fetch(input, init);
+  },
 });
 
 // ブログ一覧を取得
