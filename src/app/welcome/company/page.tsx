5a283a27abcb7f6baae66644385d555523d327c3
tomoya-okuda <tomoyaokuda.info@gmail.com>
2023-04-30 20:23:20 +0900

    globals.cssにh5登録

--- src/app/welcome/company/page.tsx
+++ src/app/welcome/company/page.tsx
@@ -1,11 +1,12 @@
-import HeadingH3 from "@/components/HeadingH3";
-import styles from 'src/app/welcome/welcome.module.scss'
+import HeadingH3 from '@/components/HeadingH3';
+import WelcomeCard from '@/components/WelcomeCard';
+import styles from 'src/app/welcome/welcome.module.scss';
 
 function page() {
   return (
     <main className={styles.main}>
       <HeadingH3 title="ようこそ、Necmosへ。" color="white" />
-      <h2>01 会社を知る</h2>
+      <WelcomeCard />
     </main>
   );
 }
---
+++ src/components/WelcomeCard.tsx
@@ -0,0 +1,12 @@
+import styles from 'src/components/styles/WelcomeCard.module.scss';
+
+function WelcomeCard() {
+  return (
+    <div className={styles.container}>
+      <h1>01</h1>
+      <h5>会社を知る</h5>
+    </div>
+  );
+} 
+
+export default WelcomeCard;
